bac72f167cf4b7a19e14046e7276dfc147f67d54
Brian Ingles <brianingles@deephaven.io>
2024-12-20 17:22:33 -0600

    Cleanup

---
+++ README.md
@@ -0,0 +1,9 @@
+# VDO Ninja Overlays
+
+Chrome extension to show username overlays in VDO Ninja.
+
+## Usage
+
+- Download repo
+- Install as unpacked extension in chrome://extensions
+- Pin extension
--- content.js
+++
@@ -1,9 +0,0 @@
-function addUserIds() {
-  const data = document.querySelectorAll("video[data-sid]");
-
-  data.forEach((datum) => {
-    datum.parentElement.dataset.userid = datum.dataset.sid;
-  });
-}
-
-addUserIds();
